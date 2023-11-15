fa4fd5afc2cf8a32496797b71cefdb856fd07e9b
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2023-11-15 22:41:22 +0300

    add counter button

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,14 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp, defineComponent} from './vendor/vue.esm-browser.js';
 
+const App = defineComponent({
+	name: 'App',
+	data() {
+		return {
+			count: 0,
+		};
+	},
+});
+
+const app = createApp(App);
+window.vm = app.mount('#app');
 // Создайте Vue приложение
