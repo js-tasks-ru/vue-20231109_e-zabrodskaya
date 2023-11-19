c78f2c8b836a2335bfe9df937cd35487ad02f860
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2023-11-19 21:35:46 +0300

    add calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,32 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp, defineComponent} from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const App = defineComponent({
+	name: 'App',
+	data() {
+		return {
+			input: {
+				number1: '',
+				number2: '',
+				radio: 'sum',
+			},
+		};
+	},
+	computed: {
+		result() {
+			if (this.input.radio === 'sum') {
+				return this.input.number1 + this.input.number2;
+			} else if (this.input.radio === 'subtract') {
+				return this.input.number1 - this.input.number2;
+			} else if (this.input.radio === 'multiply') {
+				return this.input.number1 * this.input.number2;
+			} else if (this.input.radio === 'divide') {
+				return this.input.number1 / this.input.number2;
+			}
+		},
+	},
+});
+const app = createApp(App);
+
+window.vm = app.mount('#app');
