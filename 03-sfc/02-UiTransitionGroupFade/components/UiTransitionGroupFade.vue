00aa5de60867700861b4a5b8e6096af3840140a1
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2023-11-25 21:38:19 +0300

    add 03-sfc

--- 03-sfc/02-UiTransitionGroupFade/components/UiTransitionGroupFade.vue
+++ 03-sfc/02-UiTransitionGroupFade/components/UiTransitionGroupFade.vue
@@ -17,30 +17,30 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 /* _transitions.css */
 
-.fade-list {
+.fade-list :deep(.meetups-list__item) {
   position: relative;
 }
 
-.fade-list > * {
+.fade-list :deep(.meetups-list__item) {
   opacity: 1;
   transition: opacity 0.3s ease-out;
 }
 
-.fade-list .fade-list-leave-active {
+.fade-list :deep(.meetups-list__item).fade-list-leave-active  {
   position: absolute !important;
   left: 0;
   right: 0;
 }
 
-.fade-list .fade-list-enter-from,
-.fade-list .fade-list-leave-to {
+.fade-list :deep(.meetups-list__item).fade-list-enter-from,
+.fade-list :deep(.meetups-list__item).fade-list-leave-to {
   opacity: 0;
 }
 
-.fade-list .fade-list-move {
+.fade-list :deep(.meetups-list__item).fade-list-move {
   transition: transform 0.3s;
 }
 </style>
