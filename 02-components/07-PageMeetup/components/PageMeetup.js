3aeb71a6da444c63cfb239a63587f65073d7ed79
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2023-11-25 17:50:47 +0300

    add solution for 07-PageMeetup, fix path for MeetupAgenda

--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,26 +1,80 @@
-import { defineComponent } from '../vendor/vue.esm-browser.js';
+import {defineComponent} from '../vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import {fetchMeetupById} from '../meetupService.js';
+
+import MeetupView from '../../06-MeetupView/components/MeetupView.js';
+
+const States = {
+	IDLE: 'IDLE',
+	LOADING: 'LOADING',
+	SUCCESS: 'SUCCESS',
+	ERROR: 'ERROR',
+};
 
 export default defineComponent({
-  name: 'PageMeetup',
+	name: 'PageMeetup',
 
-  components: {
-    UiAlert,
-    UiContainer,
-  },
+	States,
 
-  template: `
+	components: {
+		UiAlert,
+		UiContainer,
+		MeetupView,
+	},
+
+	props: {
+		meetupId: {
+			type: Number,
+			required: true,
+		},
+	},
+
+	data() {
+		return {
+			state: States.IDLE,
+			meetup: null,
+			error: null,
+		};
+	},
+
+	async mounted() {
+		this.fetch();
+	},
+
+	watch: {
+		meetupId() {
+			this.fetch();
+		},
+	},
+
+	methods: {
+		async fetch() {
+			this.state = States.LOADING;
+			this.meetup = null;
+			this.error = null;
+
+			try {
+				this.meetup = await fetchMeetupById(this.meetupId);
+				this.state = States.SUCCESS;
+			} catch (error) {
+				this.state = States.ERROR;
+				this.error = error.message;
+			}
+		},
+	},
+
+	template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <MeetupView v-if="state === $options.States.SUCCESS" :meetup="meetup"/>
 
-      <UiContainer>
+      <UiContainer v-if="state === $options.States.LOADING">
         <UiAlert>Загрузка...</UiAlert>
       </UiContainer>
 
-      <UiContainer>
-        <UiAlert>error</UiAlert>
+      <UiContainer v-if="state === $options.States.ERROR">
+        <UiAlert>{{error}}</UiAlert>
       </UiContainer>
+
     </div>`,
 });
