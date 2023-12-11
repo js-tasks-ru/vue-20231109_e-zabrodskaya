f78217f7e7db230b1084343038de3ef051f2cbb3
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2023-12-11 22:48:35 +0300

    add 06-03

--- 06-wrappers/03-UiInput/components/UiInput.vue
+++ 06-wrappers/03-UiInput/components/UiInput.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="input-group input-group_icon input-group_icon-left input-group_icon-right">
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+  <div class="input-group"
+    :class="{
+      'input-group_icon': $slots['left-icon'] || $slots['right-icon'],
+      'input-group_icon-left': $slots['left-icon'],
+      'input-group_icon-right': $slots['right-icon'],
+    }">
+    <div v-if="$slots['left-icon']" class="input-group__icon">
+      <slot name="left-icon" />
     </div>
 
-    <input ref="input" class="form-control form-control_rounded form-control_sm" />
+    <component :is="multiline ? 'textarea' : 'input'" ref="input"
+    :value="modelValue"
+    @[update]="$emit('update:modelValue', $event.target.value)"
+    v-bind="$attrs" class="form-control"
+    :class="{'form-control_sm' : small}, {'form-control_rounded' : rounded}"   />
 
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+    <div v-if="$slots['right-icon']" class="input-group__icon">
+      <slot name="right-icon" />
     </div>
   </div>
 </template>
@@ -15,6 +24,31 @@
 <script>
 export default {
   name: 'UiInput',
+  inheritAttrs: false,
+  props: {
+    small: Boolean,
+    rounded: Boolean,
+    multiline: Boolean,
+    modelValue: String,
+    modelModifiers: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  computed: {
+    update() {
+      if (this.modelModifiers.lazy) {
+        return 'change'
+      } else {
+        return 'input'
+      }
+    }
+  },
+  methods: {
+    focus() {
+      this.$refs['input'].focus();
+    },
+  },
 };
 </script>
 
